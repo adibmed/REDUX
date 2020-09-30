a93a9e9a5c8b6814701e01e44f1e008fd92d9bc3
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-30 01:33:56 +0000

    Simulate login/logout with redux userSlice

--- redux-demo/src/user/Login.js
+++ redux-demo/src/user/Login.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser, logout, selectUser } from "./userSlice";
 
 function Login() {
-    return (
-        <div>
-            <h3>I am the login</h3>            
-        </div>
-    )
+  const disptach = useDispatch();
+  const user = useSelector(selectUser);
+
+  const loginToApp = () => {
+    disptach(setUser({ user: "Adibe" }));
+  };
+
+  return (
+    <div>
+      <h3>I am the login</h3>
+      <button onClick={loginToApp}>Login</button>
+      <br />
+      <button onClick={() => disptach(logout())}>Logout</button>
+      <br />
+      <h1>{user ? `${user.user}` : "Nobody singnin"}</h1>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- redux-demo/src/user/userSlice.js
+++ redux-demo/src/user/userSlice.js
@@ -7,7 +7,7 @@ export const userSlice = createSlice({
   },
   reducers: {
     setUser: (state, action) => {
-      state.user = action.user;
+      state.user = action.payload.user;
     },
     logout: (state) => {
       state.user = null;
@@ -16,10 +16,7 @@ export const userSlice = createSlice({
 });
 
 export const { setUser, logout } = userSlice.actions;
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
+ 
 export const selectUser = (state) => state.user;
 
 export default userSlice.reducer;
