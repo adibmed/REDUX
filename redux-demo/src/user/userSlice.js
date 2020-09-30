cb94dd0e6a1baee0bfe549833cc2e4e41beaa2ef
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-30 01:07:38 +0000

    create userSlice [redux]

---
+++ redux-demo/src/user/userSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.user;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { setUser, logout } = userSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state) => state.counter.value)`
+export const selectUser = (state) => state.user;
+
+export default userSlice.reducer;
